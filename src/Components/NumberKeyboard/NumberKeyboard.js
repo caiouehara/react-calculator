6b5bcf146456bf403508ef8cfa7facc1e8981feb
caiouehara <caioueharamartins@gmail.com>
2019-06-20 16:21:15 -0300

    Add hook for liveNumber

--- src/Components/NumberKeyboard/NumberKeyboard.js
+++ src/Components/NumberKeyboard/NumberKeyboard.js
@@ -1,27 +1,28 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
+ 
+function NumbersKeyboard() {
+    const [liveNumber, setNumber] = useState(0);
 
-class NumbersKeyboard extends Component {
-    handleClick = (event) => {
-        console.log('isworking'+ ` ${event.target.innerHTML}`);
+    function handleClick(e) {
+        setNumber(parseInt(e.target.id));
+        console.log(liveNumber);
     };
 
-    render(){
-        return(
-            <div className="NumbersKeyboard">
-            <button className="number" onClick={this.handleClick} id='0'>0</button>
-            <button className="number" onClick={this.handleClick} id='1'>1</button>
-            <button className="number" onClick={this.handleClick} id='2'>2</button>
-            <button className="number" onClick={this.handleClick} id='3'>3</button>
-            <button className="number" onClick={this.handleClick} id='4'>4</button>
-            <button className="number" onClick={this.handleClick} id='5'>5</button>
-            <button className="number" onClick={this.handleClick} id='6'>6</button>
-            <button className="number" onClick={this.handleClick} id='7'>7</button>
-            <button className="number" onClick={this.handleClick} id='8'>8</button>
-            <button className="number" onClick={this.handleClick} id='9'>9</button>
+    return (
+        <div className="NumbersKeyboard">
+            <button className="number" onClick={handleClick} id='0'>0</button>
+            <button className="number" onClick={handleClick} id='1'>1</button>
+            <button className="number" onClick={handleClick} id='2'>2</button>
+            <button className="number" onClick={handleClick} id='3'>3</button>
+            <button className="number" onClick={handleClick} id='4'>4</button>
+            <button className="number" onClick={handleClick} id='5'>5</button>
+            <button className="number" onClick={handleClick} id='6'>6</button>
+            <button className="number" onClick={handleClick} id='7'>7</button>
+            <button className="number" onClick={handleClick} id='8'>8</button>
+            <button className="number" onClick={handleClick} id='9'>9</button>
         </div>
 
-        );
-    }
-}
+    );
+};
 
 export default NumbersKeyboard;
