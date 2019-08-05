0ad477cb5f3da5b5841105c25da44d30c41f2953
caiouehara <caioueharamartins@gmail.com>
2019-08-05 19:20:36 -0300

    Add OperationThread

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,18 @@ let operationsHistory = [];
 
 // data for Numbers and operations
 let operationsToDo = [];
+let results = [];
+// Do Operation
+function operationsThread(operationsToDo){
+    // Single Operation
+    if(operationsToDo.length === 4){
+        operationsToDo.reduce((accumulator, actualValue, index, array) => {
+            if(actualValue === "SUM"){
+                results.push(accumulator + array[index+1]);
+            }
+        });
+    }
+}
 
 const numbersThread = (screenNumber = [""], action) => {
 
@@ -16,13 +28,23 @@ const numbersThread = (screenNumber = [""], action) => {
 
     if (action.type === 'OPERATION_METHOD') {
         operationsToDo.push(parseInt(screenNumber), action.payload);
-        console.log(operationsToDo);
+        console.log(operationsToDo)
+        operationsThread(operationsToDo);
+
+        // reseting operation ( have to fix this shit )
+        if(operationsToDo.length === 4){
+            operationsToDo = [];
+            // Bugging Undefined (NAN), 4 in reduce couting, count a null.
+            results.splice(-1,1);
+            console.log(results);
+        }
+
         return screenNumber = "";
     }
 
     // Reseting Numbers
     else if (action.type === 'RESET_NUMBERS_THREAD') {
-        return screenNumber = [];
+        return screenNumber = [""];
     }
 
     return screenNumber;
